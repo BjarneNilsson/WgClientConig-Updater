4a1e5f110aaee9c9d18e7fb1558522b56944fcd5
BjarneNilsson <bjarne-disc@holmedal.net>
2023-05-25 09:32:13 +0200

    Add project files.

---
+++ WgClientConig Updater/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+using System.Net;
+
+namespace WgClientConig_Updater
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPNetwork ip = IPNetwork.Parse("2a01:799:ea9:4e01:4ecc:6aff:feb8:9270/56");
+            String Pr = ip.Network.ToString();
+            Pr = Pr.Remove(Pr.Length -4, 4)+"80";
+            Console.WriteLine(Pr);
+        }
+    }
+}
